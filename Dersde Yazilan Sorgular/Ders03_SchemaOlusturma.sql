4a800fcdb6b8409f42e97c359dc40a71b8cd9276
bulutluoz <abbulutluoz@gmail.com>
2025-11-10 21:21:01 +0100

    3.ders sonu

---
+++ Dersde Yazilan Sorgular/Ders03_SchemaOlusturma.sql
@@ -0,0 +1,15 @@
+
+-- derslerde kullanmak uzere SqlDersleri isminde bir schema olusturun
+
+CREATE SCHEMA SqlDersleri ;
+
+
+/*
+	1- Query sayfasinda bir sorgu yazilmasi, tek basina bir anlam ifade etmez
+       O sorgudan beklenen islemi yapmasi icin sorgunun calistirilmasi gerekir
+    2- SQL query sayfasindaki her komut SADECE CALISMASI GEREKTIGI KADAR calistirilmalidir
+       AMMMMAAAAA her seferinde query sayfasindaki TUM sorgularin calistirilmasi YANLIS OLUR
+	3- CALISTIRMAK ISTEDIGINIZ QUERY'i mouse ile isaretleyip 
+       sonra simsek isaretinden calistirabilirsiniz
+       
+*/
